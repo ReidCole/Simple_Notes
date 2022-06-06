7e69fe3a4b27e279bc3f254ea15df167d3cb5a72
Kininari <reid-cole@outlook.com>
2022-06-06 15:08:51 -0400

    before color change

---
+++ components/SaveModal.tsx
@@ -0,0 +1,86 @@
+import { doc, setDoc } from "firebase/firestore";
+import fileDownload from "js-file-download";
+import { nanoid } from "nanoid";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { NoteState } from "../hooks/useNoteState";
+import { firestore } from "../pages/_app";
+import { RootState } from "../redux";
+import { createNote, getCurrentLocalStorageNotes } from "../util/noteUtils";
+import Button from "./Button";
+import LoadingSpinner from "./LoadingSpinner";
+import Modal from "./Modal";
+
+type Props = {
+  isModalOpen: boolean;
+  setIsModalOpen(val: boolean): void;
+  noteState: NoteState;
+};
+
+const SaveModal: React.FC<Props> = ({ isModalOpen, noteState, setIsModalOpen }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const user = useSelector((state: RootState) => state.auth.user);
+  const router = useRouter();
+
+  function saveToLocalStorage() {
+    const notes = getCurrentLocalStorageNotes();
+    const newNote = createNote(noteState, "ls");
+    notes.push(newNote);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    router.push("/notes");
+  }
+
+  async function saveToAccount() {
+    if (user === null) return;
+    setIsLoading(true);
+
+    const newNote = createNote(noteState, user.email);
+
+    const id = nanoid();
+    const noteDoc = doc(firestore, "notes", id);
+    try {
+      await setDoc(noteDoc, newNote);
+    } catch (err) {
+      console.log(err);
+      console.log("todo: show notification");
+    }
+    setIsLoading(false);
+  }
+
+  function downloadNote() {
+    fileDownload(noteState.body, noteState.title, "text/plain");
+  }
+
+  return (
+    <>
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} heading="Save Note">
+        <Button
+          className="gap-2 bg-blue-500 text-white w-full"
+          onClick={saveToLocalStorage}
+          title="Save Note to Browser Local Storage"
+        >
+          <i className="bi-window text-xl flex" /> Save To Browser Storage
+        </Button>
+        <Button
+          className="gap-2 bg-yellow-500 text-white w-full"
+          onClick={saveToAccount}
+          title="Save Note to Your Account"
+        >
+          <i className="bi-cloud text-xl" /> Save To Account
+        </Button>
+        <Button
+          className="gap-2 bg-red-500 text-white w-full"
+          onClick={downloadNote}
+          title="Download Note as a Text File"
+        >
+          <i className="bi-download text-xl flex" /> Download as Text File
+        </Button>
+      </Modal>
+
+      <LoadingSpinner isVisible={isLoading} />
+    </>
+  );
+};
+
+export default SaveModal;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import Button from "../components/Button";
 import ButtonGroup from "../components/ButtonGroup";
 import MainContainer from "../components/MainContainer";
 import Navbar from "../components/Navbar";
 import NoteInputs from "../components/NoteInputs";
 import PageHeading from "../components/PageHeading";
 import SaveButtons from "../components/SaveButtons";
+import SaveModal from "../components/SaveModal";
 import useNoteState from "../hooks/useNoteState";
 
 const Home: NextPage = () => {
   const noteState = useNoteState();
+  const [isSaveModalOpen, setIsSaveModalOpen] = useState<boolean>(false);
 
   return (
     <MainContainer>
@@ -17,8 +21,22 @@ const Home: NextPage = () => {
       <NoteInputs noteState={noteState} />
 
       <ButtonGroup>
-        <SaveButtons noteState={noteState} />
+        <Button
+          className="gap-1.5"
+          enabledClasses="bg-green-500 text-white"
+          onClick={() => setIsSaveModalOpen(true)}
+          title="Save Note"
+          disabled={!noteState.isValid}
+        >
+          <i className="bi-save2 text-xl flex" /> Save Note
+        </Button>
       </ButtonGroup>
+
+      <SaveModal
+        isModalOpen={isSaveModalOpen}
+        setIsModalOpen={setIsSaveModalOpen}
+        noteState={noteState}
+      />
 
       <Navbar />
     </MainContainer>
